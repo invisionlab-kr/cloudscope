41ec66170386eb78fb6a649eb356c5a016e5d664
김심기 <landbirds@LANDBIRDS-MBP.local>
2023-06-24 13:20:07 +0900

    ffmpeg 디버깅

--- solo.js
+++ solo.js
@@ -1,2 +1,8 @@
 let cp = require("child_process");
 let ffmpegProcess = cp.spawn("sudo", ["ffmpeg", "-f", "v4l2", "-video_size", "1280x720", "-i", "/dev/video0", "-pix_fmt yuv420p", "-preset", "ultrafast", "-c:v", "libx264", "-b:v", "600k", "-f", "rtsp", "rtsp://localhost:8554/scope"]);
+ffmpegProcess.on("close", function(code) {
+  console.log("close", code);
+});
+ffmpegProcess.on("error", function(data) {
+  console.log("error", data);
+})
