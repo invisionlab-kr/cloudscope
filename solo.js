a2223f7b97c4f2c3518817dfe1d52d7039924726
김심기 <landbirds@LANDBIRDS-MBP.local>
2023-06-24 13:23:22 +0900

    ffmpeg 디버깅

--- solo.js
+++ solo.js
@@ -1,5 +1,5 @@
 let cp = require("child_process");
-let ffmpegProcess = cp.spawn("sudo", ["ffmpeg", "-f", "v4l2", "-video_size", "1280x720", "-i", "/dev/video0", "-pix_fmt yuv420p", "-preset", "ultrafast", "-c:v", "libx264", "-b:v", "600k", "-f", "rtsp", "rtsp://localhost:8554/scope"]);
+let ffmpegProcess = cp.exec("sudo ffmpeg -f v4l2 -video_size 1280x720 -i /dev/video0 -pix_fmt yuv420p -preset ultrafast -c:v libx264 -b:v 600k -f rtsp rtsp://localhost:8554/scope");
 ffmpegProcess.on("disconnect", function() {
   console.log("disconnected");
 });
